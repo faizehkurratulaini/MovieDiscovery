b55521b62a004b4937e49a928ea9ed9745d0951e
Faizeh Kurratul Aini <faizehkurratulaini517@gmail.com>
2024-11-11 22:16:11 +0700

    install navigasi persiapan untuk menampilkan screen

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { initializeApp } from "firebase/app";
 
 // Firebase configuration
