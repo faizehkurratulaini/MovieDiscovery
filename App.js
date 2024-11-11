d06d38c5ece50dd68d357d69488848c8d79c2726
Faizeh Kurratul Aini <faizehkurratulaini517@gmail.com>
2024-11-11 22:10:47 +0700

    install firebase

--- App.js
+++ App.js
@@ -1,6 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { initializeApp } from "firebase/app";
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC5G3Es_JaxU2o1NFH15YCoYfk-812Y_o4",
+  authDomain: "moviediscovery-ca954.firebaseapp.com",
+  projectId: "moviediscovery-ca954",
+  storageBucket: "moviediscovery-ca954.firebasestorage.app",
+  messagingSenderId: "1020152708190",
+  appId: "1:1020152708190:web:810feb65ae1cd9293948cf",
+  measurementId: "G-HBTG6TPT6M",
+};
+
+initializeApp(firebaseConfig);
 
 export default function App() {
   return (
@@ -14,8 +28,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
